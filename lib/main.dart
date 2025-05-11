fbc785ea4fad070697e158553e4b1dafc89e46a9
dimasbagas <danibaret014@gmail.com>
2025-05-11 19:04:42 +0700

    membuat themeData text style dll

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'SecondPage.dart';
 
 void main(){
   runApp(MyApp());
@@ -8,34 +7,22 @@ void main(){
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Halam Utama"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text("kehalaman kedua"),
-          onPressed: () {
-            Navigator.push(
-              context,MaterialPageRoute(builder: (context) => Secondpage()),
-            );
-          },
-        ),
+      // debugShowCheckedModeBanner: false,
+      title: "Halaman Pertama",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'Roboto',
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(fontSize: 18, color: const Color.fromARGB(221, 0, 0, 0)),
+          titleLarge: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)
+        )
       ),
     );
   }
 }
+
